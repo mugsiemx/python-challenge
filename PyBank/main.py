b6283a203a02a57d2e47ce43c430c63b96a10bf9
mugsiemx <mugsiemx@gmail.com>
2023-01-14 22:03:44 -0500

    finalizing the analysis list before screen and file output

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,77 +12,75 @@ budget_file = os.path.join("", "Resources", "budget_data.csv")
 
 # define variables
 c = ": "  # used for a better view on screen
-analysis_hdr = ("Total Months", "Total Net Amount", "Average Change",
+analysis_hdr = ("Total Months", "Total Net Amount", "Total Variance Amount", "Average Change"
                 "Greatest Increase in Profits", "Greatest Decrease in Profits")
 mon_lst = []
 pl_lst = []
+pl_var_lst = []
 analysis_lst = []
 
 pl_tmp = 0
+min_idx = 0
+max_idx = 0
+min_value = 0
+max_value = 0
 total_months = 0
 total_months_amount = 0
+total_variance_amount = 0
 total_avg_change = 0
-total_big_increase = 0
-total_big_decrease = 0
-
+# total_big_increase = f"{mon_lst} {[analysis_lst[min_idx]]}"
+# total_big_decrease = f"{mon_lst} {[analysis_lst[max_idx]]}"
+# open the .cvs file; begin defining and processing the data
 with open(budget_file, 'r') as budget_csv:
     budget_rdr = csv.reader(budget_csv, delimiter=",")
-    budget_hdr = next(budget_rdr)
     # skips the first/header row
-    # into lists:  add count number of months, add profit/loss values
+    budget_hdr = next(budget_rdr)
+    # into lists:  add count number of months, add profit/loss variances
     for row in budget_rdr:
         mon_lst.append(row[0])
-        pl_lst.append(int(row[1]))
-
+        pl_var_lst.append(int(row[1]))
+    # find the profit and loss variances, record them in the ProfitLoss Variance List
     i = 0
-    j = 1
+    j = 0
     for i in range(len(pl_lst)):
-        if i == 0 or j == len(mon_lst):
-            analysis_lst.append(0)
+        if i == 0:
+            # do nothing at the first value, keep moving
+            continue
         else:
+            # next variance less the previous variance
             pl_tmp = (int(pl_lst[i]))-(int(pl_lst[j]))
-            analysis_lst.append(pl_tmp)
+            pl_var_lst.append(int(pl_tmp))
         j += 1
-
+# match analysis values with the initial analysis header list defined in the variables
+    # The total number of months included in the dataset
     total_months = len(mon_lst)
+    analysis_lst.append(total_months)
+    # The net total amount of "Profit/Losses" over the entire period
     total_months_amount = sum(pl_lst)
-
-    # if
-    # total_months = [pl_lst].count()
-    # total_months_amount = [pl_lst].sum()
-    #  analysis_lst[i].append([pl_lst[i]-pl_tmp[1]])
-    #  pl_tmp == (int(row[1]))
-
-    #    analysis_lst.append
-#        print(pl_lst)
-    # for i, v enumerate(pl_lst):
-    #     if i != 0:
-    #         analysis_lst[i] = analysis_lst[i].append.pl_lst[i]
-    #     print(analysis_lst)
-
-# for i in range(len((analysis_lst[i+1])-(analysis_lst[i])))
-# pl_lst.append((pl_lst[i+1]=profit[i]))
-
-# sum_months = budget_df["Profit/Losses"].sum(axis=1)
-# pl_lst.append(row)
-#  for column in budget_df[]:
-#     mon_amt = budget_df.ilocindex[i]
-
-# print(pl_lst)
-
-# amount += amount
-
+    analysis_lst.append(total_months_amount)
+    # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    total_variance_amount = sum(pl_var_lst)
+    analysis_lst.append(total_variance_amount)
+    # average
+    total_avg_change = sum(pl_var_lst)/(len(mon_lst - 1))
+    analysis_lst.append(total_avg_change)
+    # check for largest increase month to month p & l variance
+    max_value = max(pl_var_lst)
+    analysis_lst[4] = pl_var_lst.index(max_value)
+    # check for largest decrease month to month p & l variance
+    min_value = min(pl_var_lst)
+    analysis_lst[5] = pl_var_lst.index(min_value)
 
 print(f"MONTHS {mon_lst}")
 print(f"BASIC {pl_lst}")
 print(f"CALCULATIONS {analysis_lst}")
 print(f"SINGLE {total_months}")
 print(f"SINGLE {total_months_amount}")
+print(f"SINGLE {total_avg_change}")
+print(f"SINGLE {total_big_increase}")
+print(f"SINGLE {total_big_decrease}")
 
 
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in profits (date and amount) over the entire period
 #
