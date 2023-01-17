9dfd22653c12b73c959fea182a43cd9f498ac426
mugsiemx <mugsiemx@gmail.com>
2023-01-16 22:47:45 -0500

    stuck on the print screen detail line

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,23 +5,28 @@
 import os
 import csv
 
-election_lst = []
-votes_lst = []
 candidate_lst = []
+election_lst = []
+votes_lst = [0, 0, 0]
 
-x = 0
+candidate_pct = 0
 candidate_votes = 0
 total_votes = 0
 idx = 0
+
+election_summary = ''
 candidate = ''
 
+# function to count the number of votes based on one vote per ballot
 
-def vote_count(candidate_lst[]):
+
+def vote_count(candidate):
     candidate_votes = candidate_lst.count(candidate)
-    return election_lst.count(candidate_votes)
+    votes_lst[idx] = candidate_votes
+    return candidate_votes, votes_lst[idx]
 
 
-# get the election data file
+# get the election data file and create the analysis file to write data
 election_file = os.path.join('', 'Resources', 'election_data.csv')
 analysis_file = os.path.join('', 'analysis', 'analysis_data.csv')
 
@@ -35,30 +40,34 @@ with open(election_file, 'r') as election_csv:
     for row in election_rdr:
         candidate_lst.append(row[2])
         total_votes += 1
-
+    # simplify the list of candidates voted for
     for name in candidate_lst:
         if name not in election_lst:
             election_lst.append(name)
-
-    for candidate in range(len(election_lst)):
+    # using a function, calculate the number of votes received per candidate
+    for candidate in election_lst:
+       # candidate_votes = 0
+        idx = int(election_lst.index(candidate))
         if candidate in candidate_lst:
-            election_lst[idx] = candidate_votes
+            vote_count(candidate)
 
-           #  c_votes = (candidate_votes[idx]) + 1
-           #  candidate_votes[idx] = c_votes
 
-print(election_lst)
-print(candidate_votes)
-print(candidate_lst)
-
-# i = 0
-
-#     election_rdr = csv.reader(election_csv, delimiter=',')
-#     # skip past the header row
-#     election_hdr = next(election_rdr)
-
-#     for row in election_rdr:  # [19719:19728]:
-#         candidate_lst = list(election_rdr)
-#         for row in candidate_lst:
-#             if candidate <> candidate_lst[row]
-#                 candidate = candidate_lst[row]
+# on screen header
+print('\n' * 3)
+print(f'-' * 30)
+print(f'Election Results')
+print(f'-' * 30)
+print(f'Total Votes: {total_votes}')
+print(f'-' * 30)
+# on screen detail
+idx = 0
+for idx in range(len(election_lst[idx])):
+    candidate_pct = int(votes_lst[idx])/{total_votes} * 100
+    election_summary = (
+        f"{election_lst[idx]}: {candidate_pct}% '('{votes_lst[idx]}')'"
+    )
+    idx += 1
+print(election_summary)
+# on screen footer
+print(f"-" * 30)
+print("\n" * 3)
