7432087268abab9f757380c0d96ed00324855ad0
mugsiemx <mugsiemx@gmail.com>
2023-01-16 19:33:50 -0500

    before test of 3 iterations

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,33 +1,64 @@
 # Python Challenge - PyPoll
 
-# created on 1/7/2023 by Sheila LaRoue
+# created on 1/15/2023 by Sheila LaRoue
 
 import os
 import csv
-# from itertools import islice
 
-# get the financial data file
-cfile = os.path.join('..', 'Resources', 'budget_data.csv')
+election_lst = []
+votes_lst = []
+candidate_lst = []
 
-row_count = 0
-# read the financial data file
-with open(cfile, encoding='utf') as csvfile:
-    rfile = csv.reader(cfile, delimiter=',')
-
-# analyze the records to make calculations:
-with open("rfile") as PollCalcs:
-    for line in PollCalcs:
-        if row_count < 10:
-            print(line)
-row_count += 1
+x = 0
+candidate_votes = 0
+total_votes = 0
+idx = 0
+candidate = ''
 
 
-# for row in budget_rdr to row[5]:
-#    if row[0] != " ":
-#       print(row[0])
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
-#
+def vote_count(candidate_lst[]):
+    candidate_votes = candidate_lst.count(candidate)
+    return election_lst.count(candidate_votes)
+
+
+# get the election data file
+election_file = os.path.join('', 'Resources', 'election_data.csv')
+analysis_file = os.path.join('', 'analysis', 'analysis_data.csv')
+
+# read the election data file
+with open(election_file, 'r') as election_csv:
+    election_rdr = csv.reader(election_csv, delimiter=',')
+    # skips the first/header row
+    election_hdr = next(election_rdr)
+    # for each row in the file create a list of only the candidates
+    # each ballot line equals one vote, sum vote number upon iteration
+    for row in election_rdr:
+        candidate_lst.append(row[2])
+        total_votes += 1
+
+    for name in candidate_lst:
+        if name not in election_lst:
+            election_lst.append(name)
+
+    for candidate in range(len(election_lst)):
+        if candidate in candidate_lst:
+            election_lst[idx] = candidate_votes
+
+           #  c_votes = (candidate_votes[idx]) + 1
+           #  candidate_votes[idx] = c_votes
+
+print(election_lst)
+print(candidate_votes)
+print(candidate_lst)
+
+# i = 0
+
+#     election_rdr = csv.reader(election_csv, delimiter=',')
+#     # skip past the header row
+#     election_hdr = next(election_rdr)
+
+#     for row in election_rdr:  # [19719:19728]:
+#         candidate_lst = list(election_rdr)
+#         for row in candidate_lst:
+#             if candidate <> candidate_lst[row]
+#                 candidate = candidate_lst[row]
